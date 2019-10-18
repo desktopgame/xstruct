00e6576863b79efdba18372823e8935c8461b0a5
desktopgame <gnndr36@gmail.com>
2019-10-18 20:00:44 +0900

    add test

--- main_test.go
+++ main_test.go
@@ -11,3 +11,13 @@ func TestConfig(t *testing.T) {
 	buf := detail.CreateProgram("./testdata/Planet.vcxproj")
 	fmt.Println(buf.String())
 }
+
+func TestFilter(t *testing.T) {
+	buf := detail.CreateProgram("./testdata/Planet.vcxproj.filters")
+	fmt.Println(buf.String())
+}
+
+func TestUser(t *testing.T) {
+	buf := detail.CreateProgram("./testdata/Planet.vcxproj.user")
+	fmt.Println(buf.String())
+}
