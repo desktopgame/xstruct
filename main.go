3191e587414839a8c7eb6f78bca3b4dbfbc93440
desktopgame <gnndr36@gmail.com>
2019-10-18 13:52:06 +0900

    test etree

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/beevik/etree"
+)
 
 func main() {
 	fmt.Println("hello")
+	edoc := etree.NewDocument()
+	err := edoc.ReadFromFile("./testdata/Planet.vcxproj")
+	if err != nil {
+		log.Fatal(err)
+	}
+	str, _ := edoc.WriteToString()
+	fmt.Println(str)
 }
