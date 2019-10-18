f8e4d82c408c346631525ad1d8e299e0d58ba833
desktopgame <gnndr36@gmail.com>
2019-10-18 21:17:51 +0900

    add flag

--- main.go
+++ main.go
@@ -1,20 +1,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
-	"os"
 
 	"github.com/desktopgame/xstruct/xstruct/detail"
 )
 
 func main() {
-	if len(os.Args) < 2 {
+	var (
+		prefix = flag.String("prefix", "", "prefix")
+		suffix = flag.String("suffix", "", "suffix")
+	)
+	flag.Parse()
+	if flag.NArg() < 1 {
 		log.Fatal("not enough argument")
 	}
-	buf := detail.CreateProgram(os.Args[1], detail.Option{
-		Prefix: "",
-		Suffix: "",
+	buf := detail.CreateProgram(flag.Arg(0), detail.Option{
+		Prefix: *prefix,
+		Suffix: *suffix,
 	})
 	fmt.Println(buf.String())
 }
